de842f246b813fc15d02929745e3e1d578dbb561
NikolayRag <nikolayr@mail.ru>
2018-04-16 03:23:05 +0300

    change: reorder KiAUTH for explicit+social logon

--- core/ki-auth.php
+++ core/ki-auth.php
@@ -33,59 +33,63 @@ include(__dir__ .'/ki-authSoc.php');
 
 
 class KiAUTH {
-	private $cbName;
-	private $socUser, $flexUser, $rights;
+	var $socUser, $flexUser, $mask=0, $rights;
 
 	var $logoutURL='', $socUrlA;
-	var $isSigned=false, $ID=0, $name='', $email='', $photoUrl='';
+	var $isSigned=false, $id=0, $name='', $email='', $photoUrl='';
 
 
-	function __construct($_db, $_settings, $_cb='logoncb'){
-		$this->cbName= $_cb;
-
-		session_start();
-
-
+	function __construct($_db, $_socialA){
+		$this->socUser= new KiSoc($_socialA);
+		$this->socUrlA= $this->socUser->urlA;
+		$this->logoutURL= $this->socUser->socialURL(0);
 
 		$this->flexUser= new \ptejada\uFlex\User();
 		$this->flexUser->config->database->pdo= $_db;
-		$this->flexUser->start();
-		if ($this->flexUser->isSigned()){
-			$this->flexUser->login(); //update from DB
-			$this->name= $this->flexUser->displayName;
-			$this->email= $this->flexUser->Email;
-			$this->photoUrl= $this->flexUser->photoURL;
+
+
+		if (!$this->initFlexUser($_db)){
+			$this->initSocUser();
 		}
 
-
-
-
-		$this->socUser= new KiSoc($_settings, $this->cbName);
-		$this->socUrlA= $this->socUser->urlA;
-
-		$this->logoutURL= $this->socUser->socialURL(0);
-
-
-        $this->isSigned= $this->flexUser->isSigned() || (bool)$this->socUser->user;
-		
-		if ($this->socUser->user){
-			$this->name= $this->socUser->user->firstName;
-			$this->email= $this->socUser->user->email;
-			$this->photoUrl= $this->socUser->user->photoUrl;
-		}
-
-		if (!$this->name)
-			$this->name= $this->email;
-
-
-
-		$this->rights= new Rights($this->flexUser->isSigned()? $this->flexUser->GroupID :0);
+		$this->isSigned= true;
 	}
+
+
+
+	function initFlexUser(){
+		if (!$this->flexUser->start()->isSigned())
+			return;
+
+
+		$this->id= $this->flexUser->ID;
+		$this->name= $this->flexUser->displayName;
+		$this->email= $this->flexUser->Email;
+		$this->photoUrl= $this->flexUser->photoURL;
+		$this->mask= $this->flexUser->mask;
+
+		return true;
+	}
+
+
+
+	function initSocUser(){
+		if (!$this->socUser->start())
+			return;
+
+		$this->email= $this->socUser->user->email;
+		$this->name= $this->socUser->user->firstName;
+		$this->photoUrl= $this->socUser->user->photoUrl;
+
+		return true;
+	}
+
 
 
 	function react($_req){
 		$this->socUser->socReact($_req);
 	}
+
 
 
 	function logout(){
