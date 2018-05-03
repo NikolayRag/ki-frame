43949277e54e6f1e5f72a8a4b9f4c214f1a17d20
NikolayRag <nikolayr@mail.ru>
2018-05-03 09:34:41 +0300

    cleanup KiSoc

--- core/ki-authSoc.php
+++ core/ki-authSoc.php
@@ -28,7 +28,7 @@ class KiSoc {
 	function __construct($_settings, $_cb='logoncb'){
 		$this->cbName= $_cb;
 
-		$this->packSettings($_settings);
+		$this->typesA= $this->packSettings($_settings);
 		$this->factory= new \Social\Factory($this->typesA);
 		$this->urlA= $this->socialFillURL();
 	}
@@ -57,40 +57,44 @@ class KiSoc {
 
 
 	function packSettings($_settings){
+		$typesA= [];
+
 		if ($_settings->VKID){
-		    $this->typesA[\Social\Type::VK] = [
+		    $typesA[\Social\Type::VK] = [
 		        'app_id' => $_settings->VKID,
 		        'secret_key' => $_settings->VKKEY,
 		        'scope' => $_settings->VKSCOPE
 		    ];
 		}
 		if ($_settings->MRID){
-		    $this->typesA[\Social\Type::MR] = [
+		    $typesA[\Social\Type::MR] = [
 		        'app_id' => $_settings->MRID,
 		        'secret_key' => $_settings->MRKEY,
 		        'scope' => $_settings->MRSCOPE
 		    ];
 		}
 		if ($_settings->FBID){
-		    $this->typesA[\Social\Type::FB] = [
+		    $typesA[\Social\Type::FB] = [
 		        'app_id' => $_settings->FBID,
 	        	'secret_key' => $_settings->FBKEY,
 		        'scope' => $_settings->FBSCOPE
 		    ];
 		}
 		if ($_settings->GITID){
-		    $this->typesA[\Social\Type::GITHUB] = [
+		    $typesA[\Social\Type::GITHUB] = [
 		        'app_id' => $_settings->GITID,
 		        'secret_key' => $_settings->GITKEY,
 		        'scope' => $_settings->GITSCOPE
 		    ];
 		}
 		if ($_settings->TWID){
-		    $this->typesA[\Social\Type::TWITTER] = [
+		    $typesA[\Social\Type::TWITTER] = [
 		        'app_id' => $_settings->TWID,
 		        'secret_key' => $_settings->TWKEY
 		    ];
 		}
+
+		return $typesA;
 	}
 
 
