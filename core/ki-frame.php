6f4dd5b0e82c6e3baa6f6f32c3ecec40b3314cb8
NikolayRag <nikolayr@mail.ru>
2018-07-10 21:42:00 +0300

    clean: retabs

--- core/ki-frame.php
+++ core/ki-frame.php
@@ -13,47 +13,47 @@ class KiFrame {
 		self::$startTime= microtime(true);
 
 /*
-include(__dir__ .'/ki-error.php');
-$ERRR= new KiERR(true);
+		include(__dir__ .'/ki-error.php');
+		$ERRR= new KiERR(true);
 
-include(__dir__ .'/init_errorh.php');
-//general error callback (to file)
-$ERRR->errCB(ErrCB\errCBFile(__dir__ .'/../log/log.txt' ));
-
-
-include(__dir__ .'/support.php');
-include(__dir__ .'/support-loose.php');
+		include(__dir__ .'/init_errorh.php');
+		//general error callback (to file)
+		$ERRR->errCB(ErrCB\errCBFile(__dir__ .'/../log/log.txt' ));
 
 
-include(__dir__ .'/ki-const.php');
-
-include(__dir__ .'/../private/c_core.php');
-include(__dir__ .'/../private/c.php');
+		include(__dir__ .'/support.php');
+		include(__dir__ .'/support-loose.php');
 
 
+		include(__dir__ .'/ki-const.php');
 
-include(__dir__ .'/ki-sql.php');
-include(__dir__ .'/ki-dict.php');
-
-include(__dir__ .'/ki-url.php');
-include(__dir__ .'/ki-client.php');
-include(__dir__ .'/ki-auth.php');
+		include(__dir__ .'/../private/c_core.php');
+		include(__dir__ .'/../private/c.php');
 
 
-if (!isset($_SESSION) && !headers_sent())
-	session_start();
 
-$DB = new PDO("mysql:host={$DBCFG->HOST};dbname={$DBCFG->NAME};charset=utf8", $DBCFG->USER, $DBCFG->PASS, array(PDO::ATTR_PERSISTENT=>true));
-$DB->exec("set names utf8");
+		include(__dir__ .'/ki-sql.php');
+		include(__dir__ .'/ki-dict.php');
 
-//additional error callback (to DB,table)
-$ERRR->errCB(ErrCB\errCBDB($DB, 'site_log_errors'));
-
-
-$URL= new KiURL($URI_ALLOW);
+		include(__dir__ .'/ki-url.php');
+		include(__dir__ .'/ki-client.php');
+		include(__dir__ .'/ki-auth.php');
 
 
-$USER= new KiAUTH($DB, $SOCIAL, $URL);
+		if (!isset($_SESSION) && !headers_sent())
+			session_start();
+
+		$DB = new PDO("mysql:host={$DBCFG->HOST};dbname={$DBCFG->NAME};charset=utf8", $DBCFG->USER, $DBCFG->PASS, array(PDO::ATTR_PERSISTENT=>true));
+		$DB->exec("set names utf8");
+
+		//additional error callback (to DB,table)
+		$ERRR->errCB(ErrCB\errCBDB($DB, 'site_log_errors'));
+
+
+		$URL= new KiURL($URI_ALLOW);
+
+
+		$USER= new KiAUTH($DB, $SOCIAL, $URL);
 */
 	}
 
