9ea58d1a939defe5847d05b3cef33a070092d2f0
NikolayRag <nikolayr@mail.ru>
2018-09-14 14:40:57 +0300

    add: KiData template

---
+++ core/KiData.php
@@ -0,0 +1,10 @@
+<?
+// -todo 71 (feature) +1: Make KiData database highlevel abstract layer
+/*
+Formalize database collection to have abstract access layer for it, like SQL functions.
+Database access function is defined once and used later.
+*/
+class KiData {
+	
+}
+?>
