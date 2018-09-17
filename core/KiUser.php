8450006ace55dc8325fdab3e2b359e6874de8db7
NikolayRag <nikolayr@mail.ru>
2018-09-17 04:22:42 +0300

    change: split KiAccount

--- core/KiUser.php
+++ core/KiUser.php
@@ -1,4 +1,5 @@
 <?
+include(__dir__ .'/kiUserAccount.php');
 include(__dir__ .'/kiUserRights.php');
 
 
@@ -56,87 +57,6 @@ Apply data from fetched uFlex user.
 
 	function accountSet($_field){
 		return $this->accountO->set($_field);
-	}
-}
-
-
-
-/*
-User Account holder
-*/
-class KiAccount {
-	private static $isInited;
-
-	private static $fieldsA;
-	private $id=0, $accountA;
-
-
-
-	function __construct(){
-		$this->init();
-
-
-		$this->accountA = [];
-		foreach (self::$fieldsA as $fName=>$fVal)
-			$this->accountA[$fName] = '';
-
-	}
-
-
-
-	function fetch($_id=0){
-		$this->id = $_id;
-
-		if (!$this->id)
-			return;
-
-
-		KiSql::apply('getAccount', $this->id);
-		while ($cFieldsA = KiSql::fetch()){
-			$fName = array_search($cFieldsA['id_field'], self::$fieldsA);
-			$this->accountA[$fName] = $cFieldsA['value'];
-		}
-	}
-
-
-
-	function init(){
-		if (self::$isInited)
-			return
-		self::$isInited = True;
-
-
-		KiSql::add('getAccountFields', 'SELECT * from users_account_fields');
-		KiSql::add('getAccount', 'SELECT * from users_account WHERE id_user=?');
-		KiSql::add('setAccount', 'REPLACE INTO users_account (id_user,id_field,value) VALUES (?,?,?)');
-
-		KiSql::apply('getAccountFields');
-
-		while ($cRow= KiSql::fetch())
-			self::$fieldsA[$cRow['name']] = $cRow['id'];
-	}
-
-
-
-	function get($_name=False){
-		if (!is_string($_name))
-			return $this->accountA;
-
-		return getA($this->accountA, $_name, '');
-	}
-
-	function set($_data=False){
-		if (!$_data)
-			return $this->accountA;
-
-		if (!is_array($_data))
-			return;
-
-		foreach (self::$fieldsA as $fName=>$cId){
-			$this->accountA[$fName] = $_data[$fName];
-			if ($this->id)
-				KiSql::apply('setAccount', $this->id, $cId, $_data[$fName]);
-		}
 	}
 }
 ?>
---
+++ core/KiUserAccount.php
@@ -0,0 +1,80 @@
+<?
+/*
+User Account holder
+*/
+class KiAccount {
+	private static $isInited;
+
+	private static $fieldsA;
+	private $id=0, $accountA;
+
+
+
+	function __construct(){
+		$this->init();
+
+
+		$this->accountA = [];
+		foreach (self::$fieldsA as $fName=>$fVal)
+			$this->accountA[$fName] = '';
+
+	}
+
+
+
+	function fetch($_id=0){
+		$this->id = $_id;
+
+		if (!$this->id)
+			return;
+
+
+		KiSql::apply('getAccount', $this->id);
+		while ($cFieldsA = KiSql::fetch()){
+			$fName = array_search($cFieldsA['id_field'], self::$fieldsA);
+			$this->accountA[$fName] = $cFieldsA['value'];
+		}
+	}
+
+
+
+	function init(){
+		if (self::$isInited)
+			return
+		self::$isInited = True;
+
+
+		KiSql::add('getAccountFields', 'SELECT * from users_account_fields');
+		KiSql::add('getAccount', 'SELECT * from users_account WHERE id_user=?');
+		KiSql::add('setAccount', 'REPLACE INTO users_account (id_user,id_field,value) VALUES (?,?,?)');
+
+		KiSql::apply('getAccountFields');
+
+		while ($cRow= KiSql::fetch())
+			self::$fieldsA[$cRow['name']] = $cRow['id'];
+	}
+
+
+
+	function get($_name=False){
+		if (!is_string($_name))
+			return $this->accountA;
+
+		return getA($this->accountA, $_name, '');
+	}
+
+	function set($_data=False){
+		if (!$_data)
+			return $this->accountA;
+
+		if (!is_array($_data))
+			return;
+
+		foreach (self::$fieldsA as $fName=>$cId){
+			$this->accountA[$fName] = $_data[$fName];
+			if ($this->id)
+				KiSql::apply('setAccount', $this->id, $cId, $_data[$fName]);
+		}
+	}
+}
+?>
