3155e88395af222b8c1385c07849630995565a06
NikolayRag <nikolayr@mail.ru>
2018-09-22 17:09:51 +0300

    prototype: KiLoad

---
+++ core/KiLoad.php
@@ -0,0 +1,14 @@
+<?
+//  todo 80 (load) +0: add KiLoad upload and download class
+class KiLoad {
+	private static $isInited;
+
+
+
+	static function init($_socialCfg){
+		if (self::$isInited)
+			return
+		self::$isInited = True;
+	}
+}
+?>
